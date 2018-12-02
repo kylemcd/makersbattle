63d4557bef3168036ff40eb0b8c6f34e43108d36
Kyle McDonald <hello@kylemcd.com>
2018-12-02 00:40:10 -0600

    Add open graph image

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -7,7 +7,12 @@ class LandingPage extends Component {
   render() {
     return (
       <Fragment>
-        <Helmet title="Makers Battle"/>
+        <Helmet title="Makers Battle">
+          <meta property="og:title" content="Makers Battle" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://makersbattle.com" />
+          <meta property="og:image" content="opengraph.png" />
+        </Helmet>
         <LandingHero />
       </Fragment>
     );
