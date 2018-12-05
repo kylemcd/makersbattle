ae74d04586ad1408eb51d0495511f20d37da4595
Kyle McDonald <hello@kylemcd.com>
2018-12-04 21:11:11 -0600

    Add node host stuff

--- src/components/EmailSubscribe/index.js
+++ src/components/EmailSubscribe/index.js
@@ -8,29 +8,51 @@ import {
   FormButton,
 } from '../../components/FormElements'
 
-import { FormContainer, EmailHeader } from './styled';
+import { 
+  FormContainer,
+  EmailHeader, 
+  Container, 
+  SponsorContainer,
+  SponsorIcon,
+  SponsorParagraph,
+  SponsorCard
+} from './styled';
 import { TwitterContainer } from '../LandingHero/styled';
 
 class EmailSubscribe extends Component {
   render() {
     return (
-      <FormContainer>
-        <EmailHeader>
-          Subscribe to be notified about upcoming competitions
-        </EmailHeader>
-        <form action="https://kylemcd.us19.list-manage.com/subscribe/post?u=5f38c3d669ea51ac7ac7d8d39&amp;id=bedf8665f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
-          <FormInputGroup>
-            <FormLabel htmlFor="mce-EMAIL">Email</FormLabel>
-            <FormField type="email" name="EMAIL" id="mce-EMAIL" placeholder="king.arthur@makersbattle.com" required/>
-          </FormInputGroup>
-          <FormButton type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"v/> 
-        </form>
-        <TwitterContainer>
-        <TwitterFollowButton
-                screenName={'makersbattle'}
-              />
-        </TwitterContainer>
-      </FormContainer>
+      <Container>
+        <SponsorContainer>
+          <SponsorCard href="https://nodehost.ca/makersbattle" target="_blank" rel="noopener noreferrer">
+            <SponsorIcon>
+              <span role="img" aria-label="blue heart">
+                💙
+              </span>
+            </SponsorIcon>
+            <SponsorParagraph>
+              Find out how we are partnering with NodeHost to help students & teachers learn code.
+            </SponsorParagraph>
+          </SponsorCard>
+        </SponsorContainer>
+        <FormContainer>
+          <EmailHeader>
+            Subscribe to be notified about upcoming competitions
+          </EmailHeader>
+          <form action="https://kylemcd.us19.list-manage.com/subscribe/post?u=5f38c3d669ea51ac7ac7d8d39&amp;id=bedf8665f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+            <FormInputGroup>
+              <FormLabel htmlFor="mce-EMAIL">Email</FormLabel>
+              <FormField type="email" name="EMAIL" id="mce-EMAIL" placeholder="king.arthur@makersbattle.com" required/>
+            </FormInputGroup>
+            <FormButton type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"v/> 
+          </form>
+          <TwitterContainer>
+          <TwitterFollowButton
+                  screenName={'makersbattle'}
+                />
+          </TwitterContainer>
+        </FormContainer>
+      </Container>
     );
   }
 }
--- src/components/EmailSubscribe/styled.js
+++ src/components/EmailSubscribe/styled.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 import { mediumBp } from '../../utils/breakpoints';
-import { black } from '../../utils/colors';
+import { black, gray } from '../../utils/colors';
 
 export const FormContainer = styled.div`
-  max-width: 400px;
-  margin: 0 auto;
   padding: 40px 0;
 
-  @media(max-width: ${mediumBp}px){
-    margin: 0 12px 40px;
+   @media(max-width: ${mediumBp}px){
     padding: 20px 0;
   }
 
@@ -23,4 +20,57 @@ export const EmailHeader = styled.h2`
 
 export const TwitterContainer = styled.div`
   padding: 20px 0;
+  margin: 0 12px 40px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  max-width: 800px;
+  flex-direction: column;
+  margin: 40px 12px 40px;
+
+  @media(min-width: ${mediumBp}px){
+    flex-direction: row;
+    margin: 0 auto;
+  }
+`;
+
+export const SponsorContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 40px;
+
+  @media(min-width: ${mediumBp}px){
+    padding: 0 40px;
+  }
+`;
+
+export const SponsorIcon = styled.div`
+  font-size: 72px;
+`;
+
+export const SponsorParagraph = styled.div`
+  padding: 0 40px;
+  color: ${black};
+  font-size: 18px;
+  line-height: 24px;
+  text-align: center;
+`;
+
+export const SponsorCard = styled.a`
+  border: 3px solid ${gray};
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px 20px;
+  text-decoration: none;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: ${gray};
+  }
 `;
